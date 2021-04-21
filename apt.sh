765b3b704afbe7a0ff3d525cf2c4bb4f208e3f30
a114tw123 <51195072+a114tw123@users.noreply.github.com>
2021-04-21 17:46:52 +0800

    Update apt.sh

--- apt.sh
+++ apt.sh
@@ -1,4 +1,7 @@
-
+mkdir -p /boot/efi/EFI/BOOT
+cp /boot/efi/EFI/debian/* /boot/efi/EFI/BOOT/
+cp /boot/efi/EFI/BOOT/grubx64.efi /boot/efi/EFI/BOOT/bootx64.efi
+ls /boot/efi/EFI/BOOT/
 apt update
 apt install xorg icewm vim net-tools gcin libappindicator3-dev \
        	fonts-noto bridge-utils gcc cpp g++ git nautilus libncurses5-dev -y
