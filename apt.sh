d17838ffc2d4726527a2e568807f969a264c4979
a114tw123 <51195072+a114tw123@users.noreply.github.com>
2021-04-21 17:43:25 +0800

    Add files via upload

---
+++ apt.sh
@@ -0,0 +1,6 @@
+
+apt update
+apt install xorg icewm vim net-tools gcin libappindicator3-dev \
+       	fonts-noto bridge-utils gcc cpp g++ git nautilus libncurses5-dev -y
+git clone https://github.com/a114tw123/eda.git
+reboot
